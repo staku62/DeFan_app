87bb4b3e33b53aff421ac31c0d16a21e1d806177
Taku Sato <gtakumi1600@gmail.com>
2020-01-10 17:17:22 +0900

    背景画像の変更

---
+++ destroy.sh
@@ -0,0 +1,1 @@
+curl -X POST https://dena-fan-app.herokuapp.com/posts/36/destroy -d '{"authenticity_token": "GJCtC1UemdxqEO2DCzOtj9;2DlllQNDIpRGtTltYvIDrRGATLV6U7PLeKIMW7admjGyuFLd98QnzgegXkx+E9Bt5w=="}'
