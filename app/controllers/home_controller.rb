63fffae63889e50dcbbf121a5c23d7c9d7db2081
staku62 <gtakumi1600@gmail.com>
2020-03-12 14:01:25 +0900

    バグ修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,13 @@ class HomeController < ApplicationController
   end
 
   def list
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(PER)
+    @posts = Post.all.search(params[:search]).page(params[:page]).per(PER)
   end
 
   def list2
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(PER)
+    @posts = Post.search(params[:search]).page(params[:page]).per(PER)
   end
 
 end
